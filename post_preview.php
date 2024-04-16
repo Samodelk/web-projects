2ab1b55b757cd04c8e1e9f5bc0769270f0454964
Samodelk <egorsamodelkin0611@gmail.com>
2024-04-16 22:12:12 +0300

    lab 5

---
+++ post_preview.php
@@ -0,0 +1,14 @@
+<div class="main-header">
+    <div class="main-header-top">
+        <h1><?= $post['title'] ?></h1>
+    </div>
+    <div class="main-header-lower-top">
+        <h3><?= $post['subtitle'] ?></h3>
+    </div>
+</div>
+<div class="main-img">
+    <img src="<?= $post['image_url'] ?>" alt="Northern lights">
+</div>
+<div class="main-text">
+    <p><?= $post['content'] ?></p>
+</div>
